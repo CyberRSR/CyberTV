77b10f04974b56b80a97cb026d4f09030127e39f
CyberRSR <molc@ya.ru>
2013-10-23 03:32:24 +0600

    Create login_check.php

---
+++ login_check.php
@@ -0,0 +1,61 @@
+<html> 
+<head> 
+<title>CyberTV</title> 
+</head> 
+<body> 
+<p align='center'>Logined</p>
+
+<?php 
+$labels = array("login" => "Login:", 
+"pass" => "password:",
+"md5pass" => "md5pass:"); 
+
+$user = array("login" => "",
+"pass" => "", 
+"md5pass" => "");
+
+foreach($_POST as $field => $value) 
+{ 
+	if($field != "submit") 
+	{ 		
+		$user[$field] = $value;
+	} 	
+} 
+
+$host = "mysql.xxx.ru"; 
+$account = "xxx"; 
+$password = "xxx"; 
+$dbname = "xxx"; 
+
+if((strlen($user[login]) > 0) && (strlen($user[pass]) > 0))
+{
+	$connect = mysqli_connect($host, $account, $password); 
+	$db = mysqli_select_db($connect, $dbname); 
+
+	$sql = "SELECT * FROM `users` WHERE `login`='$user[login]'";
+	$result = mysqli_query($connect, $sql);
+	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	if ($row['pass'] == $user['pass'])
+	{
+		echo"Login OK<br>\n";
+		foreach($row as $field => $value)
+		{
+			echo"$labels[$field] $value <br>\n";
+		}	
+	}
+	else
+	{
+		echo"Login ERROR<br>\n";
+	}
+	
+	mysqli_close($connect);
+}
+else 
+{
+	echo"Login ERROR<br>\n";
+}
+
+?>
+
+</body> 
+</html> 
