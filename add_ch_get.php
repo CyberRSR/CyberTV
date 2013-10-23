64fea2452e14acb24c4e46b9cfc268cce26d4abb
CyberRSR <molc@ya.ru>
2013-10-23 14:08:37 +0600

    add auto ch_name for pid

--- add_ch_get.php
+++ add_ch_get.php
@@ -1,4 +1,3 @@
-
 <?php 
 $params = array("md5pass" => "", "ch_name" => "", "ch_url" => "", "active" => "");
 
@@ -22,8 +21,27 @@ $row_cnt = mysqli_num_rows($result);
 if (($row_cnt > 0) && (strlen($params[ch_name]) > 0) && (strlen($params[ch_url]) > 0))
 {
 	if ($params['active'] == 1) $row[urls_added]++;
-	$sql = "UPDATE `users` SET `ch_name`='$params[ch_name]', `ch_url`='$params[ch_url]', `urls_added`='$row[urls_added]', `active`='$params[active]' WHERE `md5pass`='$params[md5pass]'";
-	$result = mysqli_query($connect, $sql);
+	if (stripos($params['ch_url'], $params['ch_name']) === false)
+	{
+		$sql = "UPDATE `users` SET `ch_name`='$params[ch_name]', `ch_url`='$params[ch_url]', `urls_added`='$row[urls_added]', `active`='$params[active]' WHERE `md5pass`='$params[md5pass]'";
+		$result = mysqli_query($connect, $sql);
+	}
+	else
+	{
+		$sql = "SELECT * FROM `users` WHERE `ch_url` LIKE '%$params[ch_name]%'";
+		$result = mysqli_query($connect, $sql);		
+		
+		if (null !== ($rown = mysqli_fetch_assoc($result)))
+		{			
+			$sql = "UPDATE `users` SET `ch_name`='_$rown[ch_name]', `ch_url`='$params[ch_url]', `urls_added`='$row[urls_added]', `active`='$params[active]' WHERE `md5pass`='$params[md5pass]'";
+			$result = mysqli_query($connect, $sql);
+		}
+		else 
+		{
+			$sql = "UPDATE `users` SET `ch_name`='$params[ch_name]', `ch_url`='$params[ch_url]', `urls_added`='$row[urls_added]', `active`='$params[active]' WHERE `md5pass`='$params[md5pass]'";
+			$result = mysqli_query($connect, $sql);
+		}
+	}
 	
 	if ($result == TRUE) echo "ch added OK <br>\n";
 	else echo "ERROR: add<br>\n";
